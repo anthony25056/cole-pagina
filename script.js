3ef010eb19a78ebcd1b01d2f09fc36a14e51caa7
Marcillo Indacochea Anthony Miguel <marcilloanthonymiguel@gmail.com>
2025-08-07 14:46:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const abrirBtns = document.querySelectorAll('.abrir-btn');
+let abierta = null;
+
+abrirBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const cardIndex = btn.getAttribute('data-card');
+    const tarjeta = document.getElementById('card' + cardIndex);
+    const parrafo = document.getElementById('parrafo' + cardIndex);
+
+    if (abierta && abierta !== cardIndex) {
+      document.getElementById('card' + abierta).classList.remove('mover');
+      document.getElementById('parrafo' + abierta).classList.remove('visible');
+      document.querySelector('.abrir-btn[data-card="' + abierta + '"]').textContent = "Abrir";
+    }
+
+    if (!tarjeta.classList.contains('mover')) {
+      tarjeta.classList.add('mover');
+      setTimeout(() => {
+        parrafo.classList.add('visible');
+        btn.textContent = "Cerrar";
+        abierta = cardIndex;
+      }, 400);
+    } else {
+      parrafo.classList.remove('visible');
+      setTimeout(() => {
+        tarjeta.classList.remove('mover');
+        btn.textContent = "Abrir";
+        abierta = null;
+      }, 400);
+    }
+  });
+});
